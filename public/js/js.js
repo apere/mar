8e7f38c4f8199e9d7549a9ee56e797c5d19e3246
adam m pere <adampere001@gmail.com>
2015-08-16 23:39:55 -0400

    styled and all cleaned up.. now it just needs some background imaages and a better color scheme

--- public/js/js.js
+++ public/js/js.js
@@ -1,4 +1,26 @@
 $(document).ready(function ($) {
+  var url = window.location.href;
+  var i = url.lastIndexOf('.html');
+  var j = url.lastIndexOf('/') + 1;
+  url = url.substring(j, i);
+  console.log(url);
+switch(url) {
+    case 'about':
+        $('#about-nav li').toggleClass('active', true);
+        break;
+    case 'service':
+        $('#service-nav li').toggleClass('active', true);
+        break;
+    case "edu":
+      $('#edu-nav li').toggleClass('active', true);
+    break;
+  case 'awards':
+      $('#awards-nav li').toggleClass('active', true);
+    break;
+    default:
+        $('#index-nav li').toggleClass('active', true);
+}
+  
   $('.mobile-button').on('click', function() {
       $('nav').toggleClass('menu-open');
   });
