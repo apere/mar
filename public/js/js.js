02757bac7ad56a2b0b489e25b6aeb672a9b81a32
adam m pere <adampere001@gmail.com>
2015-08-23 19:48:43 -0400

    style updates and backgroudn

--- public/js/js.js
+++ public/js/js.js
@@ -1,25 +1,27 @@
 $(document).ready(function ($) {
-  var url = window.location.href;
-  var i = url.lastIndexOf('.html');
-  var j = url.lastIndexOf('/') + 1;
-  url = url.substring(j, i);
-
-switch(url) {
-    case 'about':
-        $('#about-nav li').toggleClass('active', true);
-        break;
-    case 'service':
-        $('#service-nav li').toggleClass('active', true);
-        break;
-    case "edu":
-      $('#edu-nav li').toggleClass('active', true);
-    break;
-  case 'awards':
-      $('#awards-nav li').toggleClass('active', true);
-    break;
-    default:
-        $('#index-nav li').toggleClass('active', true);
-}
+  var url2 = "" + window.location.href;
+  var j = url2.lastIndexOf('/');
+  url2 = url2.substring(0, j);
+  console.log(url2);
+  j = url2.lastIndexOf('/') + 1;
+  url2 = url2.substring(j);
+  console.log(url2);
+  switch(url2) {
+      case 'about':
+          $('#about-nav li').toggleClass('active', true);
+          break;
+      case 'service':
+          $('#service-nav li').toggleClass('active', true);
+          break;
+      case "education-and-experience":
+        $('#edu-nav li').toggleClass('active', true);
+      break;
+    case 'awards-honors-press':
+        $('#awards-nav li').toggleClass('active', true);
+      break;
+      default:
+          $('#index-nav li').toggleClass('active', true);
+  }
   
   $('.mobile-button').on('click', function() {
       $('nav').toggleClass('menu-open');
